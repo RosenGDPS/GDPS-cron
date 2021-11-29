04a205d4928465b4b012d2b5e5dfa9b037659f56
j4ck <50369519+j4ck-code@users.noreply.github.com>
2021-11-29 11:47:13 +0200

    asdfasdf

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CRON_SECONDS = 3600 # 1 hour
-GDPS_DB_URL = "http://rosenqkiqbg.7m.pl/database/" # Edit this
+GDPS_DB_URL = "http://exampleGDPS.7m.pl/database/" # Edit this
 
 
 # For debugging, I suggest not turning it on:
